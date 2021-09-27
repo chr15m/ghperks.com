f63189ce24ac4972d81cf046929d00c56658816f
Chris McCormick <chris@mccormick.cx>
2021-09-28 07:20:24 +0800

    Initial version of static page.

---
+++ src/ghperks/core.cljs
@@ -0,0 +1,18 @@
+(ns ghperks.core
+  (:require [reagent.core :as r]
+            [reagent.dom :as rdom]))
+
+(defonce state (r/atom {}))
+
+(defn component-main [state]
+  [:div
+   [:h1 "ghperks"]
+   [:p "Welcome to the app!"]
+   [:button {:on-click #(js/alert "Hello world!")} "click me"]])
+
+(defn start {:dev/after-load true} []
+  (rdom/render [component-main state]
+               (js/document.getElementById "app")))
+
+(defn init []
+  (start))
