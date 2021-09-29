34a3c7cade4cf070575aa79cc7010bd9c79d0db9
Chris McCormick <chris@mccormick.cx>
2021-09-29 10:07:09 +0800

    View conditional on github auth + logout.

--- src/ghperks/server.cljs
+++ src/ghperks/server.cljs
@@ -2,6 +2,7 @@
   (:require
     [promesa.core :as p]
     [applied-science.js-interop :as j]
+    [shadow.resource :as rc]
     [sitefox.web :as web]
     [sitefox.util :refer [env reloader]]
     [ghperks.auth :as auth]))
@@ -10,21 +11,31 @@
 
 (defn setup-routes [app passport]
   (web/reset-routes app)
-  (web/static-folder app "/" (if (env "NGINX_SERVER_NAME") "build" "public"))
   (auth/setup-routes app)
-  (.get app "/hello"
-        (fn [req res] (.send res (str "Hello " (j/get-in req [:user :displayName])
-                                      " "
-                                      (j/get-in req [:user :id])
-                                      " "
-                                      (j/get-in req [:user :emails 0 :value])))))
-  (.get app "/auth/error"
-        (fn [req res] (.send res "Authentication error.")))
+  (.get app "/" (fn [req res]
+                  (if (j/get-in req [:user :displayName])
+                    (.send res (str "Hello " (j/get-in req [:user :displayName])
+                                    " "
+                                    (j/get-in req [:user :id])
+                                    " "
+                                    (j/get-in req [:user :emails 0 :value])
+                                    " "
+                                    "<a href='/auth/logout'>logout</a>"
+                                    ))
+                    (.send res (rc/inline "index.html")))))
   (.get app "/auth/github"
         (j/call passport :authenticate "github" (clj->js {:scope ["user:email"]})))
+  (.get app "/auth/logout"
+        (fn [req res]
+          (j/call req :logout)
+          (j/call-in req [:session :destroy])
+          (.redirect res "/")))
+  (.get app "/auth/error"
+        (fn [req res] (.send res "Authentication error.")))
   (.get app "/auth/github/callback"
         (j/call passport :authenticate "github" (clj->js {:failureRedirect "/auth/error"}))
-        (fn [req res] (.redirect res "/hello"))))
+        (fn [req res] (.redirect res "/")))
+  (web/static-folder app "/" (if (env "NGINX_SERVER_NAME") "build" "public")))
 
 (defn main! []
   (p/let [passport (auth/setup)
